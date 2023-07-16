28a3fb37c726ca49f358632f559a7a31d161deca
Pooriya Zarasvandi Rahimzadeh <98259649+Pooriya1998@users.noreply.github.com>
2023-07-17 03:03:36 +0330

    update

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -4,13 +4,13 @@ from blog.models import Post, Category
 register = template.Library()
 
 
-@register.inclusion_tag('blog/blog-home/post-content-area/blog-home-post-content-area-single-post-list.html')
+@register.inclusion_tag('blog/sidebar-widgets/single-post-list.html')
 def lastestposts(arg=5):
     posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
     return {'posts':posts}
 
 
-@register.inclusion_tag('blog/blog-home/post-content-area/blog-home-post-content-area-categories.html')
+@register.inclusion_tag('blog/sidebar-widgets/categories.html')
 def postcategories():
     posts = Post.objects.filter(status=1)
     categories = Category.objects.all()
