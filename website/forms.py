982111a8124ef81cf10bf01564cc00130c2e40d8
Pooriya Zarasvandi Rahimzadeh <98259649+Pooriya1998@users.noreply.github.com>
2023-07-19 10:23:48 +0330

    1. Add newsletter section to model, website application 2. Creating a newsletter section on the database 3. Creating a special newsletter form on the forms, in the website application 4. Configuring the newsletter section on URLs and Views, the website application

--- website/forms.py
+++ website/forms.py
@@ -3,13 +3,14 @@ from website.models import Contact, Newsletter
 
 
 class NameForm(forms.Form):
-    name = forms.CharField(max_length=255)
     email = forms.EmailField()
     subject = forms.CharField(max_length=255)
     message = forms.CharField(widget=forms.Textarea)
 
 
 class ContactForm(forms.ModelForm):
+    name = forms.CharField(max_length=255)
+
     class Meta:
         model = Contact
         fields = '__all__'
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http.response import HttpResponseRedirect
-from blog.models import Post, Category
-from website.forms import NameForm, ContactForm, NewsletterForm
+from blog.models import Post
+from website.forms import ContactForm, NewsletterForm
 from django.contrib import messages
 
 
@@ -15,7 +15,10 @@ def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
+            form_p = form.save(commit=False)
+            form_p.name = 'anonymous'
+            form_p.save()
+
             messages.add_message(request, messages.SUCCESS, "your ticket submited seccessfuly")
         else:
             messages.add_message(request, messages.ERROR, "your ticket didn't submited")
